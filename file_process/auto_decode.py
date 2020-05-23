053776876a402005770e956661c8a51a0b3eff92
mcfx0 <git1@mcfx.us>
2020-05-23 19:57:12 +0800

    fix chardet None encoding

--- file_process/auto_decode.py
+++ file_process/auto_decode.py
@@ -4,4 +4,6 @@ def decode(s):
 	encoding = chardet.detect(s)['encoding']
 	if encoding == 'GB2312' or encoding == 'Windows-1252':
 		encoding = 'GBK'
+	elif encoding is None:
+		encoding = 'ascii'
 	return s.decode(encoding, 'ignore')
