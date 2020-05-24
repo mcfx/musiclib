2d61f14771152c70ca66e896caddec75728c8ec6
mcfx0 <git1@mcfx.us>
2020-05-24 11:53:06 +0800

    revert changes(they cause more errors)

--- file_process/shntool.py
+++ file_process/shntool.py
@@ -7,6 +7,4 @@ def split_cue(cue_file, wave_file, dst):
 	cmd = ['shntool', 'split', '-f', cue_file, '-t', '%t', '-o', 'flac', wave_file]
 	p = Popen(cmd, stdout = PIPE, stderr = PIPE)
 	so, er = p.communicate()
-	if er != b'':
-		raise Exception(er.decode())
 	os.chdir(old_cwd)
